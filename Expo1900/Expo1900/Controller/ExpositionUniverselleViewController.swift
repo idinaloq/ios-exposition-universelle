1e7dc2414cf99a8593a30a55137b36a4f1b709fc
Hoon94 <dleognsdl1@naver.com>
2023-07-05 00:23:24 +0900

    feat: ExpositionUniverselleView 추가

--- Expo1900/Expo1900/Controller/ExpositionUniverselleViewController.swift
+++ Expo1900/Expo1900/Controller/ExpositionUniverselleViewController.swift
@@ -6,184 +6,70 @@
 
 import UIKit
 
-final class ExpositionUniverselleViewController: UIViewController {
-    private var expositionUniverselle: ExpositionUniverselle?
-    
-    private let titleLabel: UILabel = {
-        let label: UILabel = UILabel()
-        label.textAlignment = .center
-        label.numberOfLines = 2
+final class ExpositionUniverselleViewController: UIViewController, Tapable {
+    private let mainView: ExpositionUniverselleView = {
+        let view = ExpositionUniverselleView()
+//        view.translatesAutoresizingMaskIntoConstraints = false
         
-        return label
+        return view
     }()
     
-    private let visitorsLabel: UILabel = {
-        let label: UILabel = UILabel()
-        label.textAlignment = .center
-        
-        return label
-    }()
-    
-    private let locationLabel: UILabel = {
-        let label: UILabel = UILabel()
-        label.textAlignment = .center
-        
-        return label
-    }()
-    
-    private let durationLabel: UILabel = {
-        let label: UILabel = UILabel()
-        label.textAlignment = .center
-        
-        return label
-    }()
-    
-    private let totalDescriptionLabel: UILabel = {
-        let label: UILabel = UILabel()
-        label.numberOfLines = 0
-        
-        return label
-    }()
-    
-    private let posterImage: UIImageView = {
-        let imageName: String = "poster"
-        let image: UIImage? = UIImage(named: imageName)
-        let imageView: UIImageView = UIImageView(image: image)
-        
-        return imageView
-    }()
-    
-    private let leftItemImage: UIImageView = {
-        let imageName: String = "flag"
-        let image: UIImage? = UIImage(named: imageName)
-        let imageView: UIImageView = UIImageView(image: image)
-        
-        return imageView
-    }()
-    
-    private let rightItemImage: UIImageView = {
-        let imageName: String = "flag"
-        let image: UIImage? = UIImage(named: imageName)
-        let imageView: UIImageView = UIImageView(image: image)
-        
-        return imageView
-    }()
-    
-    private lazy var expositionItemListButton: UIButton = {
-        let button: UIButton = UIButton()
-        button.setTitle("한국의 출품작 보러가기", for: .normal)
-        button.setTitleColor(UIColor.blue, for: .normal)
-        button.addTarget(self, action: #selector(touchUpExpositionItemListButton), for: .touchUpInside)
-        
-        return button
-    }()
-    
-    private let contentStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.distribution = .fill
-        stackView.spacing = 8
-        
-        return stackView
-    }()
-    
-    private let buttonStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .fillEqually
-        
-        return stackView
-    }()
-    
-    private let expositionUniverselleScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        return scrollView
-    }()
+    override func loadView() {
+        view = mainView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        decodeExpositionUniverselle()
-        updateLabel()
-        configureRootView()
-        configureConstraint()
+        updateNavigationLabel()
+        updateMainViewLabel()
+        
+        mainView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = true
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.isNavigationBarHidden = false
     }
     
-    private func decodeExpositionUniverselle() {
+    private func updateNavigationLabel() {
+        navigationItem.title = "메인"
+    }
+    
+    private func updateMainViewLabel() {
+        guard let expositionUniverselle = decodeExpositionUniverselle() else {
+            return
+        }
+        
+        mainView.updateLabel(with: expositionUniverselle)
+    }
+    
+    private func decodeExpositionUniverselle() -> ExpositionUniverselle? {
+        var expositionUniverselle: ExpositionUniverselle?
+        
         do {
             expositionUniverselle = try JSONDecoder().decode(ExpositionUniverselle.self, from: JSONFile.exposition.description)
         } catch {
             print(error)
         }
+        
+        return expositionUniverselle
     }
     
-    private func updateLabel() {
-        navigationItem.title = "메인"
-        titleLabel.text = expositionUniverselle?.titleForLabel
-        visitorsLabel.text = expositionUniverselle?.visitorsForLabel
-        locationLabel.text = expositionUniverselle?.locationForLabel
-        durationLabel.text = expositionUniverselle?.durationForLabel
-        totalDescriptionLabel.text = expositionUniverselle?.totalDescription
-    }
-    
-    private func configureRootView() {
-        addArrangedSubviews(to: buttonStackView, elements: [leftItemImage,
-                                                            expositionItemListButton,
-                                                            rightItemImage
-                                                           ])
-        addArrangedSubviews(to: contentStackView, elements: [titleLabel,
-                                                             posterImage,
-                                                             visitorsLabel,
-                                                             locationLabel,
-                                                             durationLabel,
-                                                             totalDescriptionLabel,
-                                                             buttonStackView])
-        expositionUniverselleScrollView.addSubview(contentStackView)
-        view.addSubview(expositionUniverselleScrollView)
-        view.backgroundColor = .systemBackground
-    }
-    
-    private func addArrangedSubviews(to superview: UIStackView, elements subviews: [UIView]) {
-        subviews.forEach {
-            superview.addArrangedSubview($0)
-        }
-    }
-    
-    private func configureConstraint() {
-        configureExpositionUniverselleScrollViewConstraint()
-        configureContentStackViewConstraint()
-    }
-    
-    private func configureExpositionUniverselleScrollViewConstraint() {
+    private func configureMainViewConstraint() {
         NSLayoutConstraint.activate([
-            expositionUniverselleScrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            expositionUniverselleScrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            expositionUniverselleScrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            expositionUniverselleScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            mainView.topAnchor.constraint(equalTo: view.topAnchor),
+            mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            mainView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            mainView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
     }
     
-    private func configureContentStackViewConstraint() {
-        NSLayoutConstraint.activate([
-            contentStackView.topAnchor.constraint(equalTo: expositionUniverselleScrollView.topAnchor),
-            contentStackView.bottomAnchor.constraint(equalTo: expositionUniverselleScrollView.bottomAnchor),
-            contentStackView.widthAnchor.constraint(equalTo: expositionUniverselleScrollView.widthAnchor)
-        ])
-    }
-    
-    @objc private func touchUpExpositionItemListButton() {
+    @objc func touchUpExpositionItemListButton() {
         let expositionItemListViewController: ExpositionItemListViewController = ExpositionItemListViewController()
         navigationController?.pushViewController(expositionItemListViewController, animated: true)
     }
---
+++ Expo1900/Expo1900/View/ExpositionUniverselleView.swift
@@ -0,0 +1,182 @@
+//
+//  ExpositionUniverselleView.swift
+//  Expo1900
+//
+//  Created by Daehoon Lee on 2023/07/04.
+//
+
+import UIKit
+
+protocol Tapable {
+    func touchUpExpositionItemListButton()
+}
+
+class ExpositionUniverselleView: UIView {
+    var delegate: Tapable?
+    
+    private let titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 2
+        
+        return label
+    }()
+    
+    private let visitorsLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private let locationLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private let durationLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private let totalDescriptionLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let posterImage: UIImageView = {
+        let imageName: String = "poster"
+        let image: UIImage? = UIImage(named: imageName)
+        let imageView: UIImageView = UIImageView(image: image)
+        
+        return imageView
+    }()
+    
+    private let leftItemImage: UIImageView = {
+        let imageName: String = "flag"
+        let image: UIImage? = UIImage(named: imageName)
+        let imageView: UIImageView = UIImageView(image: image)
+        
+        return imageView
+    }()
+    
+    private let rightItemImage: UIImageView = {
+        let imageName: String = "flag"
+        let image: UIImage? = UIImage(named: imageName)
+        let imageView: UIImageView = UIImageView(image: image)
+        
+        return imageView
+    }()
+    
+    private lazy var expositionItemListButton: UIButton = {
+        let button: UIButton = UIButton()
+        button.setTitle("한국의 출품작 보러가기", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.addTarget(self, action: #selector(touchUpExpositionItemListButton), for: .touchUpInside)
+        button.addAction(<#T##action: UIAction##UIAction#>, for: <#T##UIControl.Event#>)
+        
+        return button
+    }()
+    
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    
+    private let buttonStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    private let expositionUniverselleScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return scrollView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureRootView()
+        configureConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func updateLabel(with data: ExpositionUniverselle) {
+        titleLabel.text = data.titleForLabel
+        visitorsLabel.text = data.visitorsForLabel
+        locationLabel.text = data.locationForLabel
+        durationLabel.text = data.durationForLabel
+        totalDescriptionLabel.text = data.totalDescription
+    }
+    
+    private func setRootViewBackground() {
+        backgroundColor = .systemBackground
+    }
+    
+    private func configureRootView() {
+        addArrangedSubviews(to: buttonStackView, elements: [leftItemImage,
+                                                            expositionItemListButton,
+                                                            rightItemImage
+                                                           ])
+        addArrangedSubviews(to: contentStackView, elements: [titleLabel,
+                                                             posterImage,
+                                                             visitorsLabel,
+                                                             locationLabel,
+                                                             durationLabel,
+                                                             totalDescriptionLabel,
+                                                             buttonStackView])
+        expositionUniverselleScrollView.addSubview(contentStackView)
+        addSubview(expositionUniverselleScrollView)
+        backgroundColor = .systemBackground
+    }
+    
+    private func addArrangedSubviews(to superview: UIStackView, elements subviews: [UIView]) {
+        subviews.forEach {
+            superview.addArrangedSubview($0)
+        }
+    }
+    
+    private func configureConstraint() {
+        configureExpositionUniverselleScrollViewConstraint()
+        configureContentStackViewConstraint()
+    }
+    
+    private func configureExpositionUniverselleScrollViewConstraint() {
+        NSLayoutConstraint.activate([
+            expositionUniverselleScrollView.topAnchor.constraint(equalTo: topAnchor),
+            expositionUniverselleScrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            expositionUniverselleScrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            expositionUniverselleScrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+    }
+    
+    private func configureContentStackViewConstraint() {
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: expositionUniverselleScrollView.topAnchor),
+            contentStackView.bottomAnchor.constraint(equalTo: expositionUniverselleScrollView.bottomAnchor),
+            contentStackView.widthAnchor.constraint(equalTo: expositionUniverselleScrollView.widthAnchor)
+        ])
+    }
+    
+    @objc private func touchUpExpositionItemListButton() {
+        delegate?.touchUpExpositionItemListButton()
+    }
+}
